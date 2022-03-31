1abcc31c38ec440c2927beaec2cdb80c38eba304
g-priime <57049481+g-priime@users.noreply.github.com>
2022-03-31 13:09:01 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const port = process.env.PORT || 5000
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
 
-app.use('/api/goals', require('./routes/goalRoutes'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+app.use("/api/goals", require("./routes/goalRoutes"));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
