0ad4a9df2b16d81ad3a15705797c43cb0a3f6bb0
g-priime <57049481+g-priime@users.noreply.github.com>
2022-03-31 11:47:08 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000
 const app = express()
 
 app.get('/api/goals', (req, res) => {
-    res.send('Get goals')
+    res.json({ message: 'Get goals' })
 })
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
