de1fb905000eebec735414dbb09917b9a18851f0
g-priime <57049481+g-priime@users.noreply.github.com>
2022-04-01 11:45:58 -0600

    generate token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -30,7 +30,8 @@ const registerUser = asyncHandler(async (req, res) => {
     const user = await User.create({
         name,
         email,
-        password: hashedPassword
+        password: hashedPassword,
+        token: generateToken(user._id),
     })
 
     if(user) {
@@ -59,6 +60,7 @@ const loginUser = asyncHandler(async (req, res) => {
             _id: user.id,
             name: user.name,
             email: user.email,
+            token: generateToken(user._id),
         })
     } else {
         res.status(400)
@@ -72,6 +74,13 @@ const loginUser = asyncHandler(async (req, res) => {
 const getMe = asyncHandler(async (req, res) => {
     res.json({ message: 'User data display'})
 })
+
+// Generate JWT
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+}
 
 module.exports = {
     registerUser,
