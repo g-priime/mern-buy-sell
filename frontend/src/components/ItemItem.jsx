ec744f022907a8c756b8c740cebb44838f8a0ab1
g-priime <57049481+g-priime@users.noreply.github.com>
2022-04-07 12:09:49 -0600

    display prices

--- frontend/src/components/ItemItem.jsx
+++ frontend/src/components/ItemItem.jsx
@@ -8,6 +8,7 @@ function ItemItem({ item }) {
     <div className="item">
       <div>{new Date(item.createdAt).toLocaleString("en-US")}</div>
       <h2>{item.text}</h2>
+      <h2>{item.price}</h2>
       <button onClick={() => dispatch(deleteItem(item._id))} className="close">
         X
       </button>
