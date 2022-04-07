6a241234b557c26f1c18113bf5e18147274e2d44
g-priime <57049481+g-priime@users.noreply.github.com>
2022-04-07 13:23:34 -0600

    modified display for items

--- frontend/src/components/ItemItem.jsx
+++ frontend/src/components/ItemItem.jsx
@@ -8,8 +8,8 @@ function ItemItem({ item }) {
     <div className="item">
       <div>{new Date(item.createdAt).toLocaleString("en-US")}</div>
       <h2>{item.text}</h2>
-      <h2>{item.price}</h2>
-      <h2>{item.username}</h2>
+      <h4>Price: ${item.price}</h4>
+      <h4>Owner: {item.username}</h4>
       <button onClick={() => dispatch(deleteItem(item._id))} className="close">
         X
       </button>
