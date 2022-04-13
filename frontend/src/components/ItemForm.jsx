474b04d8b3572ec053fb27a9f38ab182277dcaee
g-priime <57049481+g-priime@users.noreply.github.com>
2022-04-13 15:24:54 -0600

    updated ItemForm

--- frontend/src/components/ItemForm.jsx
+++ frontend/src/components/ItemForm.jsx
@@ -1,27 +1,29 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import {createItem} from '../features/items/itemSlice'
+import { createItem } from "../features/items/itemSlice";
 
 function ItemForm() {
   const [text, setText] = useState("");
   const [price, setPrice] = useState("");
-  const [category, setCategory] = useState("");
+  const [category, setCategory] = useState("Kitchen");//adds default value to category selection
   const [description, setDescription] = useState("");
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
+  let showError = false;
 
   const onSubmit = (e) => {
     e.preventDefault();
 
-    dispatch(createItem({text, price, category, description}))
-    setText('')
-    setPrice('')
-    setCategory('')
-    setDescription('')
+      dispatch(createItem({ text, price, category, description }));
+      setText("");
+      setPrice("");
+      setCategory("");
+      setDescription("");
   };
 
   return (
     <section className="form">
+      {showError ? (<>Please select a category</>) : (<></>)}
       <form onSubmit={onSubmit}>
         <div className="form-group">
           <label htmlFor="text">Item</label>
@@ -31,6 +33,7 @@ function ItemForm() {
             id="text"
             value={text}
             onChange={(e) => setText(e.target.value)}
+            required
           />
         </div>
         <div className="form-group">
@@ -41,17 +44,24 @@ function ItemForm() {
             id="price"
             value={price}
             onChange={(e) => setPrice(e.target.value)}
+            required
           />
         </div>
         <div className="form-group">
           <label htmlFor="category">Category</label>
-          <input
+          <select
             type="text"
             name="category"
             id="category"
             value={category}
             onChange={(e) => setCategory(e.target.value)}
-          />
+            required
+          >
+            <option disabled>Select category...</option>
+            <option value="Kitchen">Kitchen</option>
+            <option value="Bedroom">Bedroom</option>
+            <option value="Living Room">Living Room</option>
+          </select>
         </div>
         <div className="form-group">
           <label htmlFor="description">Description</label>
@@ -61,6 +71,7 @@ function ItemForm() {
             id="description"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
+            required
           />
         </div>
         <div className="form-group">
