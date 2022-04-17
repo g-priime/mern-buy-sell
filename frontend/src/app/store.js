38171b5c72470ff55f8365f7b7dde7b561bbad3b
g-priime <57049481+g-priime@users.noreply.github.com>
2022-04-17 14:12:41 -0600

    removed goal files from frontend

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice'
-import goalReducer from '../features/goals/goalSlice'
 import itemReducer from '../features/items/itemSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    goals: goalReducer,
     items: itemReducer,
   },
 });
