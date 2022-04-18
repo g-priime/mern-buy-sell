1c65cacc96419272c3083d6e9c768db3f40cad8a
g-priime <57049481+g-priime@users.noreply.github.com>
2022-04-18 11:10:11 -0600

    added category and date to item cards

--- frontend/src/components/ItemCard.jsx
+++ frontend/src/components/ItemCard.jsx
@@ -1,14 +1,14 @@
 function ItemCard({ item, itemButton }) {
+  let options = { year: 'numeric', month: 'long', day: 'numeric' };
 
   return (
     <>
       <div className="item">
-        {/*
-        <div>{new Date(item.createdAt).toLocaleString("en-US")}</div>
-*/}
         <h1>{item.text}</h1>
         <p className="price">${item.price}</p>
         <p>{item.description}</p>
+        <p>Category: {item.category}</p>
+        <p>Date Posted: {new Date(item.createdAt).toLocaleString("en-US", options)}</p>
         <p>{itemButton}</p>
       </div>
     </>
