44ae8fc6a8a0fcb4460edf12ca49f5c7ab28d288
g-priime <57049481+g-priime@users.noreply.github.com>
2022-04-20 13:10:25 -0600

    can not got to checkout without logged in

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -1,4 +1,37 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import Spinner from "../components/Spinner";
+import { reset } from "../features/items/itemSlice";
+
+
 function Checkout() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.auth);
+  const { isLoading, isError, message } = useSelector(
+    (state) => state.items
+  );
+
+  useEffect(() => {
+    if (isError) {
+      console.log(message);
+    }
+
+    if (!user) {
+      navigate("/login");
+    }
+
+    return () => {
+      dispatch(reset());
+    };
+  }, [user, navigate, isError, message, dispatch]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
   return (
     <div>Checkout</div>
   )
